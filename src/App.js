5d345166c580abfbdf20c6a32911465bfb0507a0
John H <johnnyhill13@gmail.com>
2020-01-03 18:13:40 -0800

    displays list of albums

--- src/App.js
+++ src/App.js
@@ -1,17 +1,82 @@
-import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
-import Amplify from 'aws-amplify';
-import aws_exports from'./aws-exports';
-import { withAuthenticator } from 'aws-amplify-react';
+import React, { Component } from "react";
+import { Grid, Header, Input, List, Segment } from "semantic-ui-react";
+import Amplify from "aws-amplify";
+import aws_exports from "./aws-exports";
+import { withAuthenticator } from "aws-amplify-react";
+import { Connect } from "aws-amplify-react";
+import { graphqlOperation } from "aws-amplify";
+
 Amplify.configure(aws_exports);
 
-class App extends Component { 
-  render() { 
+function makeComparator(key, order = "asc") {
+  return (a, b) => {
+    if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) return 0;
+
+    const aVal = typeof a[key] === "string" ? a[key].toUpperCase() : a[key];
+    const bVal = typeof b[key] === "string" ? b[key].toUpperCase() : b[key];
+
+    let comparison = 0;
+    if (aVal > bVal) comparison = 1;
+    if (aVal < bVal) comparison = -1;
+    return order === "desc" ? comparison * -1 : comparison;
+  };
+}
+
+const ListAlbums = `query ListAlbums {
+  listAlbums(limit:9999) {
+    items {
+      id
+      name
+    }
+  }
+}`;
+
+class AlbumsList extends React.Component {
+  albumItems() {
+        return this.props.albums.sort(makeComparator('name')).map(album =>
+            <li key={album.id}>
+                {album.name}
+            </li>);
+    }
+
+  render() {
     return (
-      <div>
-        <Header as='h1'>Hello World!</Header>
-      </div>
+      <Segment>
+        <Header as='h3'>My Albums</Header>
+        <List divided relaxed>
+          {this.albumItems()}
+        </List>
+      </Segment>
     );
   }
 }
-export default withAuthenticator(App, {includeGreetings: true});
+
+class AlbumsListLoader extends React.Component {
+  render() {
+    return (
+      <Connect query={graphqlOperation(ListAlbums)}>
+        {({ data, loading, errors }) => {
+          if (loading) {
+            return <div>Loading...</div>;
+          }
+          if (!data.listAlbums) return;
+
+          return <AlbumsList albums={data.listAlbums.items} />;
+        }}
+      </Connect>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Grid padded>
+        <Grid.Column>
+          <AlbumsListLoader />
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
+export default withAuthenticator(App, { includeGreetings: true });
